200b9a7dc21e9276ccb51a34715d287238cb2aa5
Kouji Matsui <k@kekyo.net>
2025-03-13 10:59:24 +0900

    Added dotnet test package.

--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Newtonsoft.Json;
+
+namespace runner_test;
+
+public readonly struct Product
+{
+    public readonly string Name;
+    public readonly DateTime Date;
+
+    public Product(string name, DateTime date)
+    {
+        this.Name = name;
+        this.Date = date;
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        var product = new Product("Apple", new DateTime(2008, 12, 28));
+        var json = JsonConvert.SerializeObject(product);
+
+        Console.WriteLine(json);
+    }
+}
